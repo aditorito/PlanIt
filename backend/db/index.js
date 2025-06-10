dd546f4a2044b93b9c52db0250372932dcde3e3a
Aditya <as0742004@gmail.com>
2025-06-10 15:30:09 +0530

    database is ready

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,92 @@
 const mongoose = require('mongoose');
 
 try {
-         mongoose.connect('mongodb+srv://AdityaPratap:qoYeY20Bkg1E45rK@cluster0.p5yi9c8.mongodb.net/PlanIt').then(()=>{
-            console.log("Database is connectde");
-            
-         })
+    mongoose.connect('mongodb+srv://AdityaPratap:qoYeY20Bkg1E45rK@cluster0.p5yi9c8.mongodb.net/PlanIt').then(() => {
+        console.log("Database is connectde");
 
-    
+    })
+
+
 } catch (error) {
-    console.log(error);    
+    console.log(error);
+}
+
+const taskSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    expense:{
+        type:Number,
+        default:0
+    },
+    assignedDate:{
+        type:Date,
+        required:true
+    },
+    dueDate:{
+        type:Date,
+        required:true
+    }
+})
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdPlans: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Plan'
+    }],
+    sharedPlans: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Plan'
+    }]
+
+})
+
+
+
+const planSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required:true
+    },
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    task: [taskSchema],
+    createdAt:{
+        type:Date,
+        default: Date.now
+    }
+});
+
+
+
+const User = mongoose.model('User', userSchema);
+const Plan = mongoose.model('Plan', planSchema);
+
+module.exports = {
+    User,
+    Plan
 }
---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 3000;
+const cors = require('cors');
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const planRouter = require('./routes/planRouter');
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/planit/auth', authRouter);
+app.use('/planit/users',userRouter);
+app.use('/planit/plan',planRouter)
+
+
+app.listen(PORT, ()=>{
+    console.log("Server is running on PORT: ", PORT);
+    
+})
+

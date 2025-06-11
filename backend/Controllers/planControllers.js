dcde13bd03b3865b2690796d8052f1e606c1ec17
Aditya <as0742004@gmail.com>
2025-06-11 16:54:22 +0530

    Now we can update plans

--- backend/Controllers/planControllers.js
+++ backend/Controllers/planControllers.js
@@ -1,79 +1,109 @@
 const { planBody } = require("../types");
 const { Plan } = require('../db');
 
-exports.getCreatedplans = async (req, res)=>{
+exports.getCreatedplans = async (req, res) => {
     try {
         const userId = req.userId;
         const plans = await Plan.find({
-            createdBy:userId
+            createdBy: userId
         });
         res.json(plans)
     } catch (error) {
         console.log(error);
         return res.status(403).json({
-            message:"kuch prioblam hai bhai"
+            message: "kuch prioblam hai bhai"
         })
-        
-        
+
+
     }
 }
-exports.getSpecificPlan = async (req, res)=>{
+exports.getSpecificPlan = async (req, res) => {
     try {
         const id = req.query.id;
         const plan = await Plan.findById(id);
         res.json(plan)
-        
+
     } catch (error) {
         console.log(error);
-           return res.status(403).json({
-            message:"kuch prioblam hai bhai"
-        })       
-        
+        return res.status(403).json({
+            message: "kuch prioblam hai bhai"
+        })
+
     }
 }
 exports.getPlanasparticipants = async (req, res) => {
     try {
         const userId = req.userId;
         const plans = await Plan.find({
-            participants:userId
+            participants: userId
         })
 
         res.json(plans)
-        
+
     } catch (error) {
-                console.log(error);
-           return res.status(403).json({
-            message:"kuch prioblam hai bhai"
-        }) 
-        
+        console.log(error);
+        return res.status(403).json({
+            message: "kuch prioblam hai bhai"
+        })
+
     }
 }
-exports.createplan = async (req,res) => {
+exports.createplan = async (req, res) => {
     try {
         const payload = req.body;
         const { success } = planBody.safeParse(payload);
         if (!success) {
             return res.json({
-                message:"Wrong format"
-            })            
-        }   
+                message: "Wrong format"
+            })
+        }
         await Plan.create({
-            title:payload.title,
-            description:payload.description,
-            createdBy:req.userId,
-            participants:payload.participants,
-            task:payload.task
+            title: payload.title,
+            description: payload.description,
+            createdBy: req.userId,
+            participants: payload.participants,
+            task: payload.task
         });
 
         res.json({
-            message:"Plan is created"
+            message: "Plan is created"
         })
 
     } catch (error) {
         console.log(error);
         res.json({
-            msg:"Somthing didn't go well with server"
-        })      
-        
+            msg: "Somthing didn't go well with server"
+        })
+
     }
 }
+
+exports.updateplan = async (req, res) => {
+    try {
+        const allowedField = ['title', 'description', 'participants'];
+        const id = req.query.id;
+        const payload = req.body;        
+        const plan = await Plan.findById(id);
+
+        for (const key of allowedField) {            
+            if (payload[key] !== undefined) {
+                plan[key] = payload[key]
+            }
+        }
+        await plan.save();
+        res.json({
+            message: "Updated successfully"
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            message: "somthing is odd"
+        })
+
+
+    }
+}
+
+exports.
--- backend/routes/planRoutes.js
+++ backend/routes/planRoutes.js
@@ -6,6 +6,7 @@ const planController = require('../Controllers/planControllers');
 planRouter.get('/', authMiddlewar, planController.getCreatedplans);
 planRouter.get('/one', authMiddlewar, planController.getSpecificPlan);
 planRouter.get('/partipant', authMiddlewar, planController.getPlanasparticipants);
+planRouter.put('/update', authMiddlewar, planController.updateplan);
 planRouter.post('/create', authMiddlewar, planController.createplan);
 
 module.exports = planRouter;
